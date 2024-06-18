65c787728b644a4c4ab3c23a7f9658b036ac1f48
mateogalache <mateogalache12@gmail.com>
2024-06-19 00:48:36 +0200

    cron

--- api/cron.ts
+++ api/cron.ts
@@ -3,9 +3,8 @@ import { generateAndUploadContent } from 'generateArticles';
 export default async function handler() {
   try {
     await generateAndUploadContent();
-    //res.status(200).json({ message: 'Artículo e imagen generados y subidos a Firebase Storage.' });
+    console.log('Artículo e imagen generados y subidos a Firebase Storage.');
   } catch (error) {
-    console.error(error);
-    //res.status(500).json({ error: 'Error al generar y subir el contenido' });
+    console.error('Error al generar y subir el contenido:', error);
   }
 }
