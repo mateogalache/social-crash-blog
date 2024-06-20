2661b37c04e4d878c074f0349c85b91ded6bdb07
Mateo Galache Guillot <mateogalache12@gmail.com>
2024-06-20 08:17:33 +0200

    xe

--- api/generateArticles.js
+++ api/generateArticles.js
@@ -91,7 +91,7 @@ async function uploadImageFromUrlToFirebaseStorage(imageUrl, destination) {
   });
   console.log(`Image uploaded to ${destination}`);
 }
-function formatDate(date) {
+function formatDate(date) { 
   const day = String(date.getDate()).padStart(2, '0');
   const month = String(date.getMonth() + 1).padStart(2, '0');
   const year = date.getFullYear();
