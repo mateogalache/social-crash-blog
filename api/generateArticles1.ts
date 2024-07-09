6ded9296da5dd6d6ccb0e4b8a9b796b3e0317ce5
mateogalache <mateogalache12@gmail.com>
2024-07-09 18:42:34 +0200

    gert

--- api/generateArticles1.ts
+++ api/generateArticles1.ts
@@ -2,14 +2,6 @@ import { temasDeportes } from '../dev/deportes.js';
 import { temasEducacion } from '../dev/educacion.js';
 import { temasEntretenimiento } from '../dev/entretenimiento.js';
 import { temasFinanzas } from '../dev/finanzas.js';
-import { temasModa } from '../dev/moda.js';
-import { temasNutricion } from '../dev/nutricion.js';
-import { temasMotor } from '../dev/motor.js';
-import { temasPolitica } from '../dev/politica.js';
-import { temasSalud } from '../dev/salud.js';
-import { temasTecnologia } from '../dev/tecnologia.js';
-import { temasTerror } from '../dev/terror.js';
-import { temasViajes } from '../dev/viajes.js';
 import { temasGaming } from '../dev/gaming.js';
 import { generateAndUploadContent } from './generateArticles.js';
 
@@ -23,18 +15,7 @@ export default async function handler () {
     temasFinanzas[day-1],
     temasGaming[day-1]
   ]
-  const newTemas2 = [  
-    temasModa[day-1],
-    temasMotor[day-1],
-    temasNutricion[day-1],
-    temasPolitica[day-1],
-    temasSalud[day-1]
-  ]
-  const newTemas3 = [ 
-    temasTecnologia[day-1],
-    temasTerror[day-1],
-    temasViajes[day-1]
-  ]
+
   const categories = [
     "deportes",
     "educacion",
@@ -42,27 +23,8 @@ export default async function handler () {
     "finanzas",
     "gaming"
   ]
-  const categories2 = [
-    "moda",
-    "motor",
-    "nutricion",
-    "politica",
-    "salud"
-  ]
-  const categories3 = [
-    "tecnologia",
-    "terror",
-    "viajes"
-  ]
-  function delay(ms:number) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  }
   try {
     generateAndUploadContent(newTemas,categories);
-    await delay(90000);
-    generateAndUploadContent(newTemas2,categories2);
-    await delay(90000);
-    generateAndUploadContent(newTemas3,categories3);
     console.log('Artículo e imagen generados y subidos a Firebase Storage.');
   } catch (error) {
     console.error('Error al generar y subir el contenido:', error);
--- api/generateArticles2.ts
+++ api/generateArticles2.ts
@@ -1,51 +1,29 @@
-import { temasDeportes2 } from '../dev/deportes2.js';
-import { temasEducacion2 } from '../dev/educacion2.js';
-import { temasEntretenimiento2 } from '../dev/entretenimiento2.js';
-import { temasFinanzas2 } from '../dev/finanzas2.js';
-import { temasModa2 } from '../dev/moda2.js';
-import { temasNutricion2 } from '../dev/nutricion2.js';
-import { temasMotor2} from '../dev/motor2.js';
-import { temasPolitica2 } from '../dev/politica2.js';
-import { temasSalud2 } from '../dev/salud2.js';
-import { temasTecnologia2 } from '../dev/tecnologia2.js';
-import { temasViajes2 } from '../dev/viajes2.js';
-import { temasGaming2 } from '../dev/gaming2.js';
+import { temasModa } from '../dev/moda.js';
+import { temasNutricion } from '../dev/nutricion.js';
+import { temasMotor } from '../dev/motor.js';
+import { temasPolitica } from '../dev/politica.js';
+import { temasSalud } from '../dev/salud.js';
 import { generateAndUploadContent } from './generateArticles.js';
 
 export default async function handler () { 
   const date = new Date();
   const day = date.getDate();
-  const newTemas = [
-    temasDeportes2[day-1],
-    temasEducacion2[day-1],
-    temasEntretenimiento2[day-1],
-    temasFinanzas2[day-1],
-    temasGaming2[day-1],
-    temasModa2[day-1],
-    temasMotor2[day-1],
-    temasNutricion2[day-1],
-    temasPolitica2[day-1],
-    temasSalud2[day-1],
-    temasTecnologia2[day-1],
-    temasViajes2[day-1]
+  const newTemas2 = [  
+    temasModa[day-1],
+    temasMotor[day-1],
+    temasNutricion[day-1],
+    temasPolitica[day-1],
+    temasSalud[day-1]
   ]
   const categories = [
-    "deportes",
-    "educacion",
-    "entretenimiento",
-    "finanzas",
-    "gaming",
     "moda",
     "motor",
     "nutricion",
     "politica",
     "salud",
-    "tecnologia",
-    "terror",
-    "viajes"
   ]
   try {
-    generateAndUploadContent(newTemas,categories)
+    generateAndUploadContent(newTemas2,categories)
     console.log('Artículo e imagen generados y subidos a Firebase Storage.');
   } catch (error) {
     console.error('Error al generar y subir el contenido:', error);
