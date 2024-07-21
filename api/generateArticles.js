155723e3cd9e60645805b1d3acdf9ddfde1fd517
mateogalache <mateogalache12@gmail.com>
2024-07-21 10:29:39 +0200

    rte

--- api/generateArticles.js
+++ api/generateArticles.js
@@ -66,7 +66,7 @@ async function uploadFileToFirebaseStorage(content, destination, contentType) {
 
 async function generateImages(prompts) {
   try {
-    const promises = prompts.map(prompt => openai.images.generate({ model: "dall-e-2", prompt: `Una imagen realista de ${prompt}` }));
+    const promises = prompts.map(prompt => openai.images.generate({ model: "dall-e-3", prompt: `Una imagen realista de ${prompt}` }));
     const responses = await Promise.all(promises);
     return responses.map(response => response.data[0].url);
   } catch (error) {
